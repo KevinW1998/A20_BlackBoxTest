6ceeb6f4defe7ef57c3693686091891a6d9b23ab
Kevin Waldock <kevin.waldock@gmail.com>
2016-02-18 10:36:25 +0100

    Added SSH url

--- src/waldock/TestMainSuite.java
+++ src/waldock/TestMainSuite.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-// SSH: 
+// SSH: git@github.com:KevinW1998/A20_BlackBoxTest.git
 
 @RunWith(Suite.class)
 @SuiteClasses({TestLadung.class, TestPersonen.class})
